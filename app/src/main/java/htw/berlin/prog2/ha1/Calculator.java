646d5d715b77104d807bddc72d52176b0c578881
HtwLong <nguyenq@htw-berlin.de>
2025-04-09 22:50:27 +0200

    Bug fix (dot after number is removed after equals button)

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -157,6 +157,7 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if(screen.endsWith(".")) screen = screen.substring(0,screen.length()-1);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
         logger.logButtonClick("=");
